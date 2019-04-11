2ffc93ec4fd309562510d91c21b680b946507ac5
Nelson Mfinda <nelsonmfinda@hotmail.com>
2019-04-11 23:27:15 +0100

    Fix build #22, database migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_08_110337) do
+ActiveRecord::Schema.define(version: 2019_04_10_024204) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2019_04_08_110337) do
     t.string "link"
     t.string "description"
     t.string "tags", array: true
+    t.string "created_by"
   end
 
   create_table "users", force: :cascade do |t|
