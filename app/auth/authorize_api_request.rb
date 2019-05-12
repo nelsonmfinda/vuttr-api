79dd787f4950e17e266685fa887256d76fb95c38
Nelson Mfinda <nelsonmfinda@hotmail.com>
2019-05-12 19:34:26 +0100

    Add exception handler for invalid token

--- app/auth/authorize_api_request.rb
+++ app/auth/authorize_api_request.rb
@@ -17,9 +17,7 @@ class AuthorizeApiRequest
     @user ||= User.find(decoded_auth_token[:user_id]) if decoded_auth_token
   rescue ActiveRecord::RecordNotFound => e
     raise(
-      ExceptionHandler::InvalidToken,
-      ("#{Message.invalid_token} #{e.message}")
-    )
+      ExceptionHandler::InvalidToken, ("#{Message.invalid_token} #{e.message}"))
   end
 
   def decoded_auth_token
