f64623c79df7b1383705e2bb7212cedadd439b1f
Nelson Mfinda <nelsonmfinda@hotmail.com>
2019-06-04 22:10:21 +0100

    Add User dependent on destroy Tools

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   has_secure_password
 
-  has_many :tools, foreign_key: :created_by
+  has_many :tools, foreign_key: :created_by, dependent: :destroy
 
   validates_presence_of :name, :email, :password_digest
   validates_uniqueness_of :email
