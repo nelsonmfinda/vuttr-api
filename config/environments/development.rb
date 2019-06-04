3e1aad645e72a692fc580f0184aadf3ff33c59cb
Nelson Mfinda <nelsonmfinda@hotmail.com>
2019-06-04 22:11:06 +0100

    Add config to use mailcatcher

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -9,6 +9,10 @@ Rails.application.configure do
   # Do not eager load code on boot.
   config.eager_load = false
 
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = { :address => "localhost", :port => 1025 }
+
   # Show full error reports.
   config.consider_all_requests_local = true
 
