b9ed4e952a69990b67a3eea8318a897e531d62ac
Nelson Mfinda <nelsonmfinda@hotmail.com>
2019-07-03 05:28:07 +0100

    Add config for coveralls and mergify

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,6 +1,9 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
+require 'coveralls'
 require "spec_helper"
 require "dox"
+
+Coveralls.wear!
 
 ENV["RAILS_ENV"] = "test"
 require File.expand_path("../../config/environment", __FILE__)
